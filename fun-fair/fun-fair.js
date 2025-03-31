1cd8aba7ad7394277f29437ce0f39aefc5bbde0f
SchulESB <emma.schnell@carl-zeiss-student.de>
2025-03-31 19:04:47 +0200

    Add files via upload

---
+++ fun-fair/fun-fair.js
@@ -0,0 +1,35 @@
+function hideMaze() {
+  var x = document.getElementById("maze-path");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideFoundItems() {
+  var x = document.getElementById("foundItems");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideTutorial() {
+  var x = document.getElementById("tutorial-text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideFightPokemonInfo() {
+  var x = document.getElementById("fight-pokemon-info");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
